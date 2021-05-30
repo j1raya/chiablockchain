5c001cc00ff6f5f1abb951e0059a310ccea30562
Matthew Poole <mattypoole@gmail.com>
2021-05-30 20:10:45 +1000

    initial commit

--- plotters_parralel.ps1
+++ plotters_parralel.ps1
@@ -1,8 +1,4 @@
-#Begin Script
-############Plotters for Temp Drive###############
-#"C:\Users\mattd\AppData\Local\chia-blockchain\app-1.1.6\resources\app.asar.unpacked\daemon\chia.exe plots create -k 32 -b 7000 -u 128 -r 6 -t E:\ -d i:\plot -n 1}"
-#invoke-expression 'cmd /c start powershell -NoExit -Command {cd "C:\Users\mattd\AppData\Local\chia-blockchain\app-1.1.6\resources\app.asar.unpacked\daemon\";$host.ui.RawUI.WindowTitle = "t1p1"; start-sleep 22000 ; .\chia.exe plots create -k 32 -b 7000 -u 128 -r 6 -t E:\ -d i:\plot -n 1 ;Read-Host -Prompt "press enter to exit"}'
-
+#Ppwershell parallel plotters, initial draft with harcoded vars
 
 $runs = 10
 $plotdelay = 32000
