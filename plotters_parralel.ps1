11e97a89931fdc34f58fb84d8092bb1c3792a6f9
Matthew Poole <mattypoole@gmail.com>
2021-06-05 00:32:54 +1000

    disk space check moved to nested within do loop

--- plotters_parralel.ps1
+++ plotters_parralel.ps1
@@ -1,17 +1,34 @@
 #Ppwershell parallel plotters, initial draft with harcoded vars
-
+Function get-freeGB ($threshold) {
+    $Volume = Get-WmiObject -Class Win32_Volume -Filter "DriveLetter = 'E:'"
+    $freeGB = $Volume.FreeSpace/1GB
+    return $freeGB
+}
+Function set-logDate {
+    $runDate = Get-Date -Format "yyyy/MM/dd HHmmss"
+    return "$runDate"
+}
 $runs = 10
-$plotdelay = 32000
+$plotdelay = 30000
 $i = 1
- do{
-    If ($alreadyRun) {
-        Start-Sleep $plotdelay
-        invoke-expression 'cmd /c start powershell -NoExit -Command {C:\Users\mattd\AppData\Local\chia-blockchain\app-1.1.6\resources\app.asar.unpacked\daemon\chia plots create -k 32 -b 7000 -u 128 -r 6 -t E:\ -d i:\plot -n 1}'
-    }
-    Else {
-        $alreadyRun = $true
-        invoke-expression 'cmd /c start powershell -NoExit -Command {C:\Users\mattd\AppData\Local\chia-blockchain\app-1.1.6\resources\app.asar.unpacked\daemon\chia plots create -k 32 -b 7000 -u 128 -r 6 -t E:\ -d i:\plot -n 1}'
-    }
+Function freeGBr {
+    $freeGBr = [math]::Round((get-freeGB),2)
+    return $freeGBr
+}
+do{
+while ((get-freeGB) -le '290') {
+    Write-Output ([string](set-logDate) + [string](" : Not enough space (" + $(freeGBr) + " GB) - pausing for 5 mins"))
+    Start-Sleep 300
+    } 
+    Write-Output ([string](set-logDate) + [string](" : Free space is  " + $(freeGBr) + " GB - continuing."))
+    Write-Output ([string](set-logDate) + [string](" : starting loop $i"))
+    invoke-expression 'cmd /c start powershell -NoExit -Command {C:\Users\mattd\AppData\Local\chia-blockchain\app-1.1.6\resources\app.asar.unpacked\daemon\chia plots create -k 32 -b 7000 -u 128 -r 6 -t E:\ -d i:\karuma -n 1}'
+    #$plotTime = measure-command {invoke-expression 'cmd /c start powershell -NoExit -Command {C:\Users\mattd\AppData\Local\chia-blockchain\app-1.1.6\resources\app.asar.unpacked\daemon\chia plots create -k 32 -b 7000 -u 128 -r 6 -t E:\ -d i:\karuma -n 1}'}
+    #$plotTimesecs = $plotTime.TotalSeconds
+    #wrote-output [string](Get-Date -Format "yyyy/MM/dd HHmmss") + [string](" : Last run $plotTimesecs, adjusting next plot delay")
+    Start-Sleep $plotdelay
+    #clear-variable plotTimesecs
     $i++
     }
- while ($i -ne $runs)
+while ($i -ne $runs)
+clear-Variable i
